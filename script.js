eb896c249ddda323cf44f5ec2b4946555a23d2cd
joannawchwang <joannawch0301@gmail.com>
2024-06-20 13:24:19 -0500

    work time

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 let button = document.querySelector("button");
 let q1answer = ("Rat");
 let q2answer = ("Stag");
+let q3answer = ("Avada Kedavra");
 
 button.addEventListener("click", function(){
     let firstInput = document.querySelector(".firstAnswer").value;
@@ -10,13 +11,12 @@ button.addEventListener("click", function(){
 
     if (firstInput === q1answer && secondInput === q2answer){
         result.innerHTML = "You are killing this game!";
-        
-    } else if (firstInput !== q1answer || secondInput === q2answer){
+    } else if (firstInput !== q1answer && secondInput === q2answer){
         result.innerHTML = "Fair game, you probably read/watched Harry Potter a long time ago.";
-    } else if (firstInput === q1answer || secondInput !== q2answer){
+    } else if (firstInput === q1answer && secondInput !== q2answer){
         result.innerHTML = "Fair game, you probably read/watched Harry Potter a long time ago.";
     } else {
-        result.innerHTML = "Have you really read/watched Harry Potter before?"
+        result.innerHTML = "Have you really read/watched Harry Potter before?";
     }
 
     if (firstInput === "" || secondInput === "") {
